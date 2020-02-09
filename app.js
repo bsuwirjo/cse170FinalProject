0cf2ae75f2fe0e7d4f900e241a8b41d41518875a
Alex <abiz@ucsd.edu>
2020-02-09 15:37:04 -0800

    fleshed out doc listeners

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 var path = require('path');
+import $ from jQuery;
 
-app.use(express.static('public'));
-app.use('/css',express.static(__dirname +'/css'));
+$(document).ready(function() {
+    // all custom jQuery will go here
+    $("div.home").click(function(){
+        alert( "Handler for .click() called." );
+
+    });
+});
+
 app.use(express.static(__dirname + '/public'));
+app.use('/css',express.static(__dirname +'/css'));
+app.use('/js',express.static(__dirname +'/js'));
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
---
+++ js/listener.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // all custom jQuery will go here
+    $(".home").click(function(){
+        alert( "Handler for .click() called." );
+    });
+});
