46902aef97f9c1d6873ae4884562df403beb95e3
Alex <abiz@ucsd.edu>
2020-02-09 19:04:49 -0800

    bla bla have screens done

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var path = require('path');
 app.use(express.static(__dirname + '/public'));
 app.use('/css',express.static(__dirname +'/css'));
 app.use('/js',express.static(__dirname +'/js'));
+app.use('/views', express.static(__dirname +'/views'));
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
@@ -14,4 +15,7 @@ app.get('/addStock.html', function(req,res){
     res.sendFile(path.join(__dirname + '/addStock.html'));
 });
 
+app.get('/settings', function(req,res) {
+    res.sendFile(path.join(__dirname + '/views/settings.html'));
+} )
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
