4af270560c458037bcd883e001858dfef39e9e3a
Alex <abiz@ucsd.edu>
2020-02-11 18:43:02 -0800

    changed file structure

--- app.js
+++ app.js
@@ -3,28 +3,26 @@ var hbs = require( 'express-handlebars');
 const app = express()
 const port = 3000
 var path = require('path');
+var index = require('./routes/index');
 
-
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 app.use('/css',express.static(__dirname +'/css'));
 app.use('/js',express.static(__dirname +'/js'));
 app.use('/views', express.static(__dirname +'/views'));
+app.use('/open-iconic', express.static(__dirname +'/open-iconic'));
+
+
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + index.view));
 })
 app.get('/addStock.html', function(req,res){
     res.sendFile(path.join(__dirname + '/addStock.html'));
 });
 
 // view engine setup
-app.set('view engine', 'hbs');
 
-app.engine( 'hbs', hbs( {
-  extname: 'hbs',
-  defaultView: 'default',
-  layoutsDir: __dirname + '/views/pages/',
-  partialsDir: __dirname + '/views/partials/'
-}));
+
 
 app.get('/settings', function(req,res) {
     res.sendFile(path.join(__dirname + '/views/settings.html'));
